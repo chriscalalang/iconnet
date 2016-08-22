21f3c835c300b79b0cba2752be0be44f545b6431
Sydney Joi Agcaoili <sydneyagcaoili@gmail.com>
2016-08-22 23:43:23 +0800

    hide other contents

--- app/src/main/java/ph/edu/bulsu/compnetworkingapp/activities/TopicContentActivity.java
+++ app/src/main/java/ph/edu/bulsu/compnetworkingapp/activities/TopicContentActivity.java
@@ -7,6 +7,7 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v4.app.ActivityOptionsCompat;
+import android.text.Html;
 import android.util.Log;
 import android.view.View;
 import android.webkit.WebView;
@@ -22,6 +23,7 @@ import ph.edu.bulsu.compnetworkingapp.IconNetApplication;
 import ph.edu.bulsu.compnetworkingapp.R;
 import ph.edu.bulsu.compnetworkingapp.constants.BundleIDs;
 import ph.edu.bulsu.compnetworkingapp.models.Topic;
+import ph.edu.bulsu.compnetworkingapp.utils.DeviceUtils;
 
 /**
  * Created by Sheychan on 6/17/2016.
@@ -29,11 +31,11 @@ import ph.edu.bulsu.compnetworkingapp.models.Topic;
 public class TopicContentActivity extends HidingToolbarActivity {
 
     private Topic topic;
-    private TextView tvText;
     private WebView wvHtml;
     private LinearLayout llImages;
     private RelativeLayout rlImages;
     private ProgressBar pbImages;
+    private LinearLayout llContent;
 
     @Override
 
@@ -51,14 +53,59 @@ public class TopicContentActivity extends HidingToolbarActivity {
 
         setTitle(topic.getTitle());
 
+        llContent = (LinearLayout) findViewById(R.id.llContent);
         rlImages = (RelativeLayout) findViewById(R.id.rlImages);
         llImages = (LinearLayout) findViewById(R.id.llImages);
         pbImages = (ProgressBar) findViewById(R.id.pbImages);
-        tvText = (TextView) findViewById(R.id.tvText);
 
 
         Log.e("Images", "" + topic.getImages().size());
         Log.e("Tags", "" + topic.getTags().size() + " with content" + topic.getTags().toString());
+
+//        setupSlideShowImages();
+        pbImages.setVisibility(View.GONE);
+
+
+        setupTextAndImageCards();
+
+
+        wvHtml = (WebView) findViewById(R.id.wvHtml);
+        wvHtml.getSettings();
+        wvHtml.setBackgroundColor(Color.TRANSPARENT);
+        wvHtml.loadDataWithBaseURL(topic.getBaseFolderPath(), topic.getHtml(), "text/html", "UTF-8", "");
+    }
+
+    private void setupTextAndImageCards() {
+        String topicText = topic.getText();
+
+        int index = 0;
+        String subtext;
+        llContent.removeAllViews();
+        do {
+            boolean containsCard = topicText.contains("<card />");
+            subtext = topicText.substring(0, containsCard ? topicText.indexOf("<card />") : topicText.length());
+
+            View view = getLayoutInflater().inflate(R.layout.item_text_card, llContent, false);
+            TextView tvText = (TextView) view.findViewById(R.id.tvText);
+            ImageView ivImage = (ImageView) view.findViewById(R.id.ivImage);
+
+            tvText.setText(Html.fromHtml(subtext));
+
+            if (index < topic.getImages().size()) {
+                Glide.with(TopicContentActivity.this).load(Uri.parse(topic.getBaseFolderPath() + topic.getImages().get(index))).fitCenter().into(ivImage);
+            } else {
+                ivImage.setVisibility(View.GONE);
+            }
+            index++;
+            llContent.addView(view);
+
+            topicText = topicText.substring(subtext.length() + (containsCard ? 8 : 0));
+        } while (topicText.length() > 0);
+
+
+    }
+
+    private void setupSlideShowImages() {
 
         if (topic.getImages().size() < 1) {
             rlImages.setVisibility(View.GONE);
@@ -84,12 +131,6 @@ public class TopicContentActivity extends HidingToolbarActivity {
             }
         }
 
-        tvText.setText(topic.getText());
-
-        wvHtml = (WebView) findViewById(R.id.wvHtml);
-        wvHtml.getSettings();
-        wvHtml.setBackgroundColor(Color.TRANSPARENT);
-        wvHtml.loadDataWithBaseURL(topic.getBaseFolderPath(), topic.getHtml(), "text/html", "UTF-8", "");
     }
 
     @Override
--- app/src/main/java/ph/edu/bulsu/compnetworkingapp/utils/DeviceUtils.java
+++ app/src/main/java/ph/edu/bulsu/compnetworkingapp/utils/DeviceUtils.java
@@ -1,7 +1,10 @@
 package ph.edu.bulsu.compnetworkingapp.utils;
 
+import android.content.Context;
 import android.content.res.Resources;
+import android.util.DisplayMetrics;
 import android.util.TypedValue;
+import android.view.WindowManager;
 
 import ph.edu.bulsu.compnetworkingapp.IconNetApplication;
 
@@ -16,5 +19,22 @@ public class DeviceUtils {
         float px = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, r.getDisplayMetrics());
 
         return (int) px;
+    }
+
+
+    public static int getDeviceWidth() {
+        DisplayMetrics metrics = new DisplayMetrics();
+        WindowManager windowManager = (WindowManager) IconNetApplication.getInstance().getApplicationContext().getSystemService(Context.WINDOW_SERVICE);
+        windowManager.getDefaultDisplay().getMetrics(metrics);
+
+        return metrics.widthPixels;
+    }
+
+    public static int getDeviceHeight() {
+        DisplayMetrics metrics = new DisplayMetrics();
+        WindowManager windowManager = (WindowManager) IconNetApplication.getInstance().getApplicationContext().getSystemService(Context.WINDOW_SERVICE);
+        windowManager.getDefaultDisplay().getMetrics(metrics);
+
+        return metrics.heightPixels;
     }
 }
