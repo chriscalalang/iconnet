6aa6f354e69c20f9611d6468879025b33bc8216f
Sydney Joi R. Agcaoili <sydneyagcaoili@gmail.com>
2016-12-02 01:01:12 +0800

    update ui

--- app/src/main/java/ph/edu/bulsu/compnetworkingapp/activities/QuizIPCalculatorActivity.java
+++ app/src/main/java/ph/edu/bulsu/compnetworkingapp/activities/QuizIPCalculatorActivity.java
@@ -2,6 +2,8 @@ package ph.edu.bulsu.compnetworkingapp.activities;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 import ph.edu.bulsu.compnetworkingapp.R;
 import ph.edu.bulsu.compnetworkingapp.fragments.IPCalculatorFragment;
@@ -13,8 +15,29 @@ public class QuizIPCalculatorActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz_ipcalculator);
 
+        setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
         getSupportFragmentManager().beginTransaction().replace(R.id.flContent, IPCalculatorFragment.newInstance(false)).commit();
 
 
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed() {
+        finish();
     }
 }
